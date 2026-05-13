fb170d8bc4bd87afdec68d5fffa685179ae24b4c
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-05-13 13:51:24 +0100

    Update Create.sql

--- scripts/Bronze/Create.sql
+++ scripts/Bronze/Create.sql
@@ -1,9 +1,10 @@
 -- Creating tables for each csv files
 
 -- query to create claim_type table
+drop table if exists MC_silver.claim_type;
 create table claim_type(
 ID int Primary key,
-cost_claims_year NVARCHAR(50),
-cost_claim_by_type NVARCHAR(50),
+cost_claims_year VARCHAR(50),
+cost_claim_by_type VARCHAR(50),
 claim_type VARCHAR(50)
 )
