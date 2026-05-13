1ed15fa84149b30f88286bbad15c8731b5453629
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-05-13 14:46:03 +0100

    Update bronze_load.sql

--- scripts/Bronze/bronze_load.sql
+++ scripts/Bronze/bronze_load.sql
@@ -1,1 +1,8 @@
-
+-- Query to load claim_type data 
+truncate table mc_bronze.claim_type;
+load data local infile '/Users/yeswanthbattina/Downloads/sample type claim.csv'
+into table mc_bronze.claim_type
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
