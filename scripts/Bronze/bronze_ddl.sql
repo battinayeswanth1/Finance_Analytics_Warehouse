46def2a8fd4310aaca5011f830e60393f0b0ef62
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-05-13 14:16:47 +0100

    Update bronze_ddl.sql

--- scripts/Bronze/bronze_ddl.sql
+++ scripts/Bronze/bronze_ddl.sql
@@ -3,7 +3,7 @@
 -- query to create claim_type table
 drop table if exists mc_bronze.claim_type;
 create table claim_type(
-ID int Primary key,
+ID int,
 cost_claims_year VARCHAR(50) CHARACTER SET utf8mb4,
 cost_claim_by_type VARCHAR(50) CHARACTER SET utf8mb4,
 claim_type VARCHAR(50)
