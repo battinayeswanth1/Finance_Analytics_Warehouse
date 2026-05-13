38f17c58715c971c9e5a9eadbc978fc7e6eaea8d
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-05-13 14:48:00 +0100

    Update bronze_ddl.sql

--- scripts/Bronze/bronze_ddl.sql
+++ scripts/Bronze/bronze_ddl.sql
@@ -8,3 +8,29 @@ cost_claims_year VARCHAR(50) CHARACTER SET utf8mb4,
 cost_claim_by_type VARCHAR(50) CHARACTER SET utf8mb4,
 claim_type VARCHAR(50)
 )
+
+-- CREATING MOTOR_VEHICLE_INSURANCE_DATA table
+drop table if exists mc_bronze.mv_insurance;
+create table mv_insurance(
+ID INT,
+contract_str_date 			VARCHAR(50),
+last_renewal_date 			VARCHAR(50),
+next_renewal_date 			VARCHAR(50),
+dob 			  			VARCHAR(50),
+dl_issue_date 	  			VARCHAR(50),
+distribution_channel		INT,
+seniortiy_years 			INT,
+policy_running_years 		INT,
+max_policies 				INT,
+premium 					VARCHAR(50),
+claim_cost_yearly 			VARCHAR(50),
+nclaim_history 				INT,
+second_driver 				VARCHAR(50),
+veh_registered_year 		VARCHAR(50),
+veh_power 					INT,
+veh_cylinder_cap 			INT,
+veh_value 					VARCHAR(50),
+veh_no_doors 				INT,
+veh_fuel_type 				VARCHAR(50),
+veh_weight 					INT
+)CHARACTER SET utf8mb4;
