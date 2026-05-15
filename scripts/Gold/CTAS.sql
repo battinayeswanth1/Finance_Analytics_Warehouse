7d83f34e964f8d9089b603ab36b5c0270f66a299
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-05-15 19:58:26 +0100

    Update CTAS.sql

--- scripts/Gold/CTAS.sql
+++ scripts/Gold/CTAS.sql
@@ -1,1 +1,11 @@
 
+-- Creating a star schema
+-- Creating a dimension table for customer using CTAS
+drop table if exists mc_gold.dim_customer;
+create table mc_gold.dim_customer as(
+select 
+	ID,
+    dob as customer_DOB,
+    dl_issue_date as customer_DL_issue_date,
+    seniortiy_years as cutomer_driving_experience
+from mc_silver.mv_insurance
