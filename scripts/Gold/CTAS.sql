9668f41723022ae6a900af0734c803039f2bc375
Yeswanth Battina <battinayeswanth1@gmail.com>
2026-05-15 20:03:47 +0100

    Update CTAS.sql

--- scripts/Gold/CTAS.sql
+++ scripts/Gold/CTAS.sql
@@ -9,3 +9,18 @@ select
     dl_issue_date as customer_DL_issue_date,
     seniortiy_years as cutomer_driving_experience
 from mc_silver.mv_insurance
+
+-- Creating a dimension table for vehicle using CTAS
+	drop table if exists mc_gold.dim_vehicle;
+create table mc_gold.dim_vehicle as(
+select 
+	ID,
+    veh_registered_year as vehicle_registered_year,
+    veh_power as vehicle_power,
+    veh_value as vehicle_value,
+    veh_no_doors as vehicle_no_doors,
+    veh_fuel_type as vehicle_fuel_type,
+    veh_lenght as vehicle_lenght,
+    veh_weight as vehicle_weight
+from mc_silver.mv_insurance
+)
